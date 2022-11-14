91797fd0eaee1d2eec0c0c41d25a80478797720e
AykutSarac <aykutsarac0@gmail.com>
2022-11-14 22:34:28 +0300

    fix csp

--- ext-src/extension.ts
+++ ext-src/extension.ts
@@ -66,30 +66,31 @@ async function initJsonCrack(context: vscode.ExtensionContext) {
   const mainScript = manifest.files["main.js"];
   const mainStyle = manifest.files["main.css"];
 
+  const basePathOnDisk = vscode.Uri.file(
+    path.join(context.extensionPath, "build")
+  );
   const scriptPathOnDisk = vscode.Uri.file(
     path.join(context.extensionPath, "build", mainScript)
   );
-  const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
   const stylePathOnDisk = vscode.Uri.file(
     path.join(context.extensionPath, "build", mainStyle)
   );
-  const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
+
+  // const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
+
+  //const baseUri = panel.webview.asWebviewUri(basePathOnDisk);
+  const stylesMainUri = panel.webview.asWebviewUri(stylePathOnDisk);
+  const scriptUri = panel.webview.asWebviewUri(scriptPathOnDisk);
+
   const nonce = getNonce();
 
   panel.webview.html = `<!DOCTYPE html>
   <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <meta name="theme-color" content="#000000">
-    <title>React App</title>
-    <link rel="stylesheet" type="text/css" href="${styleUri}">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
-    <base href="${vscode.Uri.file(
-      path.join(context.extensionPath, "build")
-    ).with({
-      scheme: "vscode-resource",
-    })}/">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'unsafe-inline' 'unsafe-eval' vscode-resource: data: https: http:;">
+
+    <link href="${stylesMainUri}" rel="stylesheet">
   </head>
   <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
